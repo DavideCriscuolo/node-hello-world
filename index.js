9cd486448da14c9400eb8b503f2d25a388aa700a
DavideCriscuolo <davide.alcatel@gmail.com>
2025-06-26 15:51:49 +0200

    aggiunto axios come dipendenza e implementata una chiamata API per ottenere un'email casuale

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 console.log("Hello Boolean");
 import chalk from "chalk";
+import axios from "axios";
 console.log(chalk.bgGreen("Chalk installato"));
 
 console.log(process.env.My_Secret_Key);
 console.log(process.argv[2]);
+axios
+  .get("https://flynn.boolean.careers/exercises/api/random/mail")
+  .then((response) => {
+    console.log(response.data);
+  });
