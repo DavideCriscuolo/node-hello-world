8278d75050368372c8fe13c83e369fc13a028609
DavideCriscuolo <davide.alcatel@gmail.com>
2025-06-26 13:27:01 +0200

    usato il process.argv per passare un argomento inserito dall utente

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log("Hello Boolean");
-
 import chalk from "chalk";
 console.log(chalk.bgGreen("Chalk installato"));
 
 console.log(process.env.My_Secret_Key);
+console.log(process.argv[2]);
